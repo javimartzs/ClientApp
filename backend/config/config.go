9f6f2919c1b9b71d446bd27cef7207fdc27696fd
Javi Martinez <javier.martinez.snt@gmail.com>
2024-10-26 20:43:23 +0200

    Add config file

---
+++ backend/config/config.go
@@ -0,0 +1,39 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+var Vars Config
+
+type Config struct {
+	DBUser string
+	DBPass string
+	DBName string
+	DBPort string
+	DBHost string
+	JwtKey string
+}
+
+func LoadEnv() {
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading env file: %v", err)
+	}
+
+	Vars = Config{
+		DBUser: os.Getenv("DB_USER"),
+		DBPass: os.Getenv("DB_PASS"),
+		DBName: os.Getenv("DB_NAME"),
+		DBPort: os.Getenv("DB_PORT"),
+		DBHost: os.Getenv("DB_HOST"),
+		JwtKey: os.Getenv("JWT_KEY"),
+	}
+
+	fmt.Println("Environments var imported")
+}
